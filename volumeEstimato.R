b719d3aa5eb5bc11d02c585918184f3d56dd11b3
Alyssa Green <18arg5@queensu.ca>
2023-01-10 19:41:22 -0500

    Fix spelling error

--- volumeEstimato.R
+++ volumeEstimato.R
@@ -5,7 +5,7 @@ measurements=read.csv("measurements.csv")
 library(dplyr)
 
 #Add new column called volume, where volume of limbs is estimated using volume of a cylinder
-measurements=measurements%>%mutate(Organisim, Limb_width, Limb_length, Observer, Volume=(Limb_width/2)^2*Limb_length*pi)
+measurements=measurements%>%mutate(Organism, Limb_width, Limb_length, Observer, Volume=(Limb_width/2)^2*Limb_length*pi)
 
 #Export to a CSV file called measurements.csv
 write.csv(measurements,"./measurements.csv", row.names = F)
